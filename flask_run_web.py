84537a7b7fd7e35e2c67a3cb84079a26fae3642b
zhangshuhui241 <zhangshuhui241@qq.com>
2019-11-25 14:16:30 +0800

    update flask_run_web.py for keyword function implementation

--- flask_run_web.py
+++ flask_run_web.py
@@ -4,17 +4,25 @@ import pandas as pd
 import numpy as np
 from kashgari import utils
 from flask import Flask, url_for, render_template, request, redirect, session
+from keywordMapping import keyWordMapping
 
 app = Flask(__name__)
 
 model_path = './chat_model_serving/1'
 url = "http://127.0.0.1:8501/v1/models/gru_bert:predict"
 
-print('loading question loading file ... ', end = '')
+print('loading question mapping file ... ', end = '')
 answer_mapping_file = 'answer.csv'
 answer = pd.read_csv(answer_mapping_file,sep=',',encoding='utf-8')
 answer = answer.groupby('question').agg('first')
 print('complete!')
+
+print('loading keyword mapping file', end = ' ... ')
+keywordEngine = keyWordMapping()
+keywordEngine.load_config_file('./data/keywords.csv')
+km_question = '胸痛什么时候明显一些'
+km_label = keywordEngine.judge(km_question)
+print('test question = ', km_question, ', label = ',km_label)
 
 def get_ai_reply(sentence,
                  model_path='./chat_model_serving/1',
@@ -36,7 +44,12 @@ def ai():
     question=request.args.get('question')
     label,prob,reply = get_ai_reply(question,model_path = model_path,url = url,answer = answer)
     print(label,prob,reply)
-    result = reply + '  (question label：' + str(label) + ', probability：' + str(int(prob*100)) + '%)'
+    result = reply
+    result = result + '\n(label_ai：' + str(label) + ', probability：' + str(int(prob*100)) + '%)'
+    
+    km_label = keywordEngine.judge(question)
+    result = result + '\n(label_keyword: ' + str(km_label)
+    
     return result
 
 @app.route('/wx', methods=['GET','POST'])
